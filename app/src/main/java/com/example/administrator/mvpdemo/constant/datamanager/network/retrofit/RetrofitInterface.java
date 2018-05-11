3590815130a13d587b7635a628afd63f60c4258c
wangsheng <wangsheng@gionee.com>
2018-05-11 16:34:58 +0800

    更新框架

--- app/src/main/java/com/example/administrator/mvpdemo/constant/datamanager/network/retrofit/RetrofitInterface.java
+++ app/src/main/java/com/example/administrator/mvpdemo/constant/datamanager/network/retrofit/RetrofitInterface.java
@@ -1,10 +1,6 @@
 package com.example.administrator.mvpdemo.constant.datamanager.network.retrofit;
 
 
-import com.example.administrator.mvpdemo.constant.bean.Book;
-import com.example.administrator.mvpdemo.constant.bean.PhoneData;
-
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
